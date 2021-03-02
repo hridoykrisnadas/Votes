6beb18a0889948c972fe2bc1406b57667fdb5842
Hridoy <hridoykrisnadas@gmail.com>
2021-03-02 11:54:07 +0600

    Master

---
+++ app/src/main/java/com/shitsolution/votes/Voter_Verify.java
@@ -0,0 +1,15 @@
+package com.shitsolution.votes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Voter_Verify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voter__verify);
+
+    }
+}
